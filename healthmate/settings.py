e8165fe94cb333ff03c1cff397c8b97046e9de55
manish23 <manish.181623@ncit.edu.np>
2023-06-15 17:03:56 +0545

    added change password

--- healthmate/settings.py
+++ healthmate/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'healthapp.apps.HealthappConfig'
+    'healthapp.apps.HealthappConfig',
+    'crispy_forms'
+]
+
+CRISPY_TEMPLATE_PACK=[
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
